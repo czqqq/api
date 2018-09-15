82e372ad3304a6b0778e04c6ebb43abff0fe0f0b
czqiang <czqiang_1@163.com>
2018-09-15 10:50:12 +0800

    基本文件夹提交

--- src/main/java/com/api/ApiApplication.java
+++ src/main/java/com/api/ApiApplication.java
@@ -1,9 +1,11 @@
 package com.api;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.api.dao")
 public class ApiApplication {
 
     public static void main(String[] args) {
