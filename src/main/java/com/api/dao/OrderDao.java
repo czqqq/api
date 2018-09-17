d23c1c1fa7a02d13a59d4953e24290341c3aa4d3
zhiyong.cai@ucarinc.com <zhiyong.cai@ucarinc.com>
2018-09-17 11:27:50 +0800

    订单表调整

--- src/main/java/com/api/dao/OrderDao.java
+++ src/main/java/com/api/dao/OrderDao.java
@@ -1,9 +1,16 @@
 package com.api.dao;
 
+import com.api.model.Order;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderDao {
 
     public void deleteById(Long id);
+    public Order selectById(Long id);
+    public List<Order> selectByIds(List<Long> ids);
+    public void update(Order order);
+    public void updateSelective(Order order);
 }
--- src/main/java/com/api/service/impl/OrderServiceImpl.java
+++ src/main/java/com/api/service/impl/OrderServiceImpl.java
@@ -4,6 +4,8 @@ import com.api.dao.OrderDao;
 import com.api.dao.OrderDetailDao;
 import com.api.dao.ProductDao;
 import com.api.dao.ProductTypeDao;
+import com.api.model.Order;
+import com.api.model.OrderDetail;
 import com.api.model.vo.OrderDetailVo;
 import com.api.model.vo.OrderVo;
 import com.api.service.OrderService;
@@ -27,13 +29,30 @@ public class OrderServiceImpl implements OrderService {
     private ProductTypeDao productTypeDao;
 
     @Override
-    public Long modifyOrder(OrderVo order, OrderDetailVo details) {
+    public Long modifyOrder(OrderVo order, OrderDetailVo detail) {
+        Order orderPo;
+        OrderDetail detailPo ;
         if (order.getId()!=null){
             //编辑
+            orderPo = orderDao.selectById(order.getId());
+            detailPo  = new OrderDetail();
+           if(orderPo!=null){
+               covertOrderVoToPo(order,detail,orderPo,detailPo);
+           }
+           orderDao.update(orderPo);
+
         }else{
             //新增
+            orderPo = new Order();
+            detailPo  = new OrderDetail();
+            covertOrderVoToPo(order,detail,orderPo,detailPo);
         }
+
         return order.getId();
+    }
+
+    private void covertOrderVoToPo(OrderVo order, OrderDetailVo detail, Order orderPo, OrderDetail detailPo) {
+
     }
 
     @Override
