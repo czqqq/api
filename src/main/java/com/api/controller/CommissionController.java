cfc753f239c9d0fa165775f290be5dd2c831595e
czqiang <czqiang_1@163.com>
2018-10-31 23:54:40 +0800

    图片上传、佣金接口

---
+++ src/main/java/com/api/controller/CommissionController.java
@@ -0,0 +1,10 @@
+package com.api.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommissionController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+}
---
+++ src/main/java/com/api/controller/PictureController.java
@@ -0,0 +1,67 @@
+package com.api.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+public class PictureController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @PostMapping("/upload")
+    @ResponseBody
+    public String upload(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return "上传失败，请选择文件";
+        }
+
+        String fileName = file.getOriginalFilename();
+        String filePath = "/Users/itinypocket/workspace/temp/";
+        File dest = new File(filePath + fileName);
+        try {
+            file.transferTo(dest);
+            logger.info("上传成功");
+            return "上传成功";
+        } catch (IOException e) {
+            logger.error(e.toString(), e);
+        }
+        return "上传失败！";
+    }
+
+    @PostMapping("/multiUpload")
+    @ResponseBody
+    public String multiUpload(HttpServletRequest request) {
+        List<MultipartFile> files = ((MultipartHttpServletRequest) request).getFiles("file");
+        String filePath = "F://pic//banner//";
+        for (int i = 0; i < files.size(); i++) {
+            MultipartFile file = files.get(i);
+            if (file.isEmpty()) {
+                return "上传第" + (i++) + "个文件失败";
+            }
+            String fileName = file.getOriginalFilename();
+
+            File dest = new File(filePath + fileName);
+            try {
+                file.transferTo(dest);
+                logger.info("第" + (i + 1) + "个文件上传成功");
+            } catch (IOException e) {
+                logger.error(e.toString(), e);
+                return "上传第" + (i++) + "个文件失败";
+            }
+        }
+
+        return "上传成功";
+
+    }
+
+}
